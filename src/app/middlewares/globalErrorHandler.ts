747911a1021d9c61c2f2b0f7e9d1a1125a319322
Nirob_Barman <niroborin111@gmail.com>
2024-06-15 17:25:25 +0600

    Sample Error Response

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -14,7 +14,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = 500;
   let message = "Something went wrong!";
 
-  let errorSources: TErrorSources = [
+  let errorMessages: TErrorSources = [
     {
       path: "",
       message: "Something went wrong",
@@ -27,31 +27,30 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    errorMessages = simplifiedError?.errorSources;
     // console.log(simplifiedError);
   } else if (err?.name === "ValidationError") {
     // console.log("Mongoose Validation Error");
     const simplifiedError = handleValidationError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    errorMessages = simplifiedError?.errorSources;
     // console.log(simplifiedError);
   } else if (err?.name === "CastError") {
     const simplifiedError = handleCastError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    errorMessages = simplifiedError?.errorSources;
     // console.log(simplifiedError);
   } else if (err?.code === 11000) {
     const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
-    // console.log(simplifiedError);
+    errorMessages    // console.log(simplifiedError);
   } else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
-    errorSources = [
+    errorMessages = [
       {
         path: "",
         message: err.message,
@@ -59,7 +58,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     ];
   } else if (err instanceof Error) {
     message = err.message;
-    errorSources = [
+    errorMessages = [
       {
         path: "",
         message: err.message,
@@ -71,7 +70,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     success: false,
     message,
     // error: err.message,
-    errorSources,
+    errorMessages,
     // error: err,
     stack: config.NODE_ENV === "development" ? err?.stack : null,
   });
